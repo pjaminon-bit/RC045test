be969d22b1d9768ba546dc74290a8ac1bba8b60d
pjaminon-bit <pjaminon@me.com>
2026-08-18 16:41:38 +0200

    Use standard public navigation on baanreglement

--- baanreglement.php
+++ baanreglement.php
@@ -2,4 +2,6 @@
 // Generieke contentpagina-route: de URL blijft baanreglement.php/baanreglement.html,
 // maar layout en inhoud worden opgebouwd via het paginatype "artikelen".
 require_once __DIR__ . '/content-renderer.php';
+require_once __DIR__ . '/content-publieke-nav.php';
+contentPubliekeNavStartFilter('baanreglement', rc045Taal());
 contentRenderArtikelen('baanreglement');
