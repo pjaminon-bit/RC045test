1401213612c9cfc8fdc8acabff7052e2dea2fd1e
pjaminon-bit <pjaminon@me.com>
2026-08-18 16:41:51 +0200

    Use standard public navigation on Ontstaan

--- ontstaan.php
+++ ontstaan.php
@@ -2,4 +2,6 @@
 // Generieke contentpagina-route: de URL blijft ontstaan.php/ontstaan.html,
 // maar layout en inhoud worden opgebouwd via het paginatype "verhaal".
 require_once __DIR__ . '/content-renderer.php';
+require_once __DIR__ . '/content-publieke-nav.php';
+contentPubliekeNavStartFilter('ontstaan', rc045Taal());
 contentRenderVerhaal('ontstaan');
